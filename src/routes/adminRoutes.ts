d3c003dee68b63aaf40f4176e56aa86ebc22aa8f
bmchien1 <chienbui10052003@gmail.com>
2024-12-06 20:32:48 +0700

    create contest admin

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,9 +1,13 @@
-import {CreateProblemPage} from "@/pages";
+import {CreateProblemPage, CreateContestPage} from "@/pages";
 
 const adminRoutes = {
 	createProblem: {
 		path: '/admin/problem/create',
 		component: CreateProblemPage,
+	},
+	createContest: {
+		path: '/admin/contest/create',
+		component: CreateContestPage,
 	},
 }
 
