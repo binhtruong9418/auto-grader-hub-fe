14667d68f1dcd5861afba0517d8041c4229509f3
bmchien1 <chienbui10052003@gmail.com>
2024-12-11 07:32:12 +0700

    logo

--- src/apis/service/userContestService.ts
+++ src/apis/service/userContestService.ts
@@ -2,6 +2,8 @@ import axiosClient from "@/apis/config/axiosClient.ts";
 
 const userContestService = {
 	getAll: async (query: any = {}): Promise<any> => {
+		const response = await axiosClient.get("/api/user-contest/me", {params: query});
+		console.log(response);
 		return await axiosClient.get("/api/user-contest/me", {params: query});
 	},
 }
